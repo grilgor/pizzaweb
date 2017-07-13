12960744aa0fde421503bc0f6a591875cf381c14
Edvis <edvinas.petrosius@gmail.com>
2017-07-13 18:27:40 +0300

    no message

--- scripts/javascript.js
+++ scripts/javascript.js
@@ -1,14 +1,10 @@
-(document).ready(function(){
-    (".row").removeClass("offset");
-});/**
- * Created by Grimmjow on 7/4/2017.
- */
+
 
 
 function myMap() {
     var mapOptions = {
         center: new google.maps.LatLng(41.878114, -87.629798),
-        zoom: 10,
+        zoom: 15,
         mapTypeId: google.maps.MapTypeId.HYBRID
     }
     var map = new google.maps.Map(document.getElementById("map"), mapOptions);
