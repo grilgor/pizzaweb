ab473e4b4896977c454f3c3410a4b8647840e805
Edvis <edvinas.petrosius@gmail.com>
2017-07-15 20:36:14 +0300

    no message

--- scripts/javascript.js
+++ scripts/javascript.js
@@ -1,6 +1,3 @@
-
-
-
 function myMap() {
     var mapOptions = {
         center: new google.maps.LatLng(41.878114, -87.629798),
@@ -9,3 +6,27 @@ function myMap() {
     }
     var map = new google.maps.Map(document.getElementById("map"), mapOptions);
 }
+
+var divs = ["pizzas", "salads", "starter"];
+var visibleDivId = null;
+function menuChange(divId) {
+    if (visibleDivId === divId) {
+
+    } else {
+        visibleDivId = divId;
+    }
+    hideNonVisibleDivs();
+}
+function hideNonVisibleDivs() {
+    var i, divId, div;
+    for (i = 0; i< divs.length; i++) {
+        divId = divs[i];
+        div = document.getElementById(divId);
+        if (visibleDivId === divId) {
+            div.style.display = "block";
+        } else {
+            div.style.display = "none";
+        }
+    }
+}
+
